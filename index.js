c3e283ca2e1f990dbc198a74997c2e70ccfe257b
Masonhealy <67674776+Masonhealy@users.noreply.github.com>
2020-07-01 15:14:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
 
-bot.on('ready', () =>{
-    console.log('The BOT is online!');
-    bot.user.setActivity('his dad code', { type: 'WATCHING'});
-})
 
-bot.on('guildMemberAdd', member =>{
 
-    const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
-    if(!channel) return;
 
-    channel.send(`Welcome to our server, ${member}, You better not be a gay fag or I will hunt you down `)
-});
+
+
+
+
+
+
+
+
+
 
 
 
