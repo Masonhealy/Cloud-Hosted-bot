f44d3b48f29f6afc421d6f2741033e44401390a6
Masonhealy <67674776+Masonhealy@users.noreply.github.com>
2020-07-01 08:50:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bot = new Discord.Client();
 
 bot.on('ready', () =>{
     console.log('The BOT is online!');
-bot.user.setActivity('Fuck Pride!', { type: 'WATCHING'});
+bot.user.setActivity('To its master', { type: 'LISTENING'});
+bot.user.setStatus('idle')
 })
 
 bot.login(process.env.token);
