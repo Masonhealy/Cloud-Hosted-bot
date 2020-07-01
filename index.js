373f3d5e07c93caee7e47dc580a00159e7162254
Masonhealy <67674776+Masonhealy@users.noreply.github.com>
2020-07-01 10:35:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on('ready', () =>{
 bot.on('guildMemberAdd', member =>{
 
     const channel = member.guild.channels.cache.find(channel => channel.name === "welcome")
-    if (channel) return;
+    if (!channel) return;
 
     channel.send('Welcome to our server, ${member}, You better not be a gay fag or i will hunt you down lul ')
 });
