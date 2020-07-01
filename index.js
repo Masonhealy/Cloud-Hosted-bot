fe0567b9174769e426e56496c76e70492bfddea0
Masonhealy <67674776+Masonhealy@users.noreply.github.com>
2020-07-01 09:05:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 bot.on('ready', () =>{
     console.log('The BOT is online!');
-bot.user.setActivity('W8x0 Code mad shit', { type: 'WATCHING'});
+bot.user.setActivity('w8x0 code mad shit', { type: 'WATCHING'});
 bot.user.setStatus('idle')
 })
 
